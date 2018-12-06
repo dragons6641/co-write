77736c0238c180e401a7a3e445468cdc53fcd9bb
s009255 <s0092555@gmail.com>
2018-12-06 23:03:52 +0900

    Add files via upload

--- routes/non.js
+++ routes/non.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var path = require('path');
 var template = require('../lib/template.js');
+var db = require('../lib/db');
 
 
 var name = '';
@@ -20,7 +21,7 @@ router.get('/Main',function(req,res){
   if(req.session.user_id){
     user_id = req.session.user_id;
     username = req.session.username;
-    res.render('nonsul/Main');
+    res.render('nonsul/Main', {username:username});
   }
   else{
     res.redirect('/auth/login');
@@ -28,12 +29,12 @@ router.get('/Main',function(req,res){
 });
 
 router.get('/Solve',function(req,res){
-  res.render('nonsul/Solve');
+  res.render('nonsul/Solve', {username:username});
 });
 
 router.post('/Solve2',function(req,res){
   name = req.body.uni;
-  res.render('nonsul/Solve2');
+  res.render('nonsul/Solve2', {username:username});
 });
 
 router.post('/Solve2_process', function(req,res){
@@ -67,7 +68,7 @@ router.get('/Solve2_download', function(req,res){
 
 router.get('/Solve3',function(req,res){
   year = req.body.year;
-  var html = template.Solve3(prob_count);
+  var html = template.Solve3(prob_count, username);
   res.send(html);
 });
 
@@ -93,11 +94,11 @@ router.post('/Solve4',function(req,res){
     });
   }
   
-  res.render('nonsul/Solve4')
+  res.render('nonsul/Solve4', {username:username})
 });
 
 router.get('/Annotation',function(req,res){
-  res.render('nonsul/Annotation')
+  res.render('nonsul/Annotation', {username:username})
 });
 
 router.post('/Annotation2',function(req,res){
@@ -109,7 +110,7 @@ router.post('/Annotation2',function(req,res){
         console.log(err)
       }
       else{
-        var html = template.Annotation2(result2.length,result2);
+        var html = template.Annotation2(result2.length,result2, req.session.username);
         res.send(html);
       }
     })
@@ -138,37 +139,37 @@ router.post('/Annotation3',function(req,res){
 
     });
   }
-  res.render('nonsul/Annotation3')  
+  res.render('nonsul/Annotation3', {username:username})  
 });
 
 router.get('/My_page',function(req,res){
-  
   db.query(`SELECT * FROM essay_p where problem_num in (select problem_num from essay_s where user_id = ${req.session.user_id} group by problem_num );`, function(err,result1){
-    db.query(`SELECT * FROM essay_p where problem_num in (select problem_num from essay_a where adviser_id = ${req.session.user_id} group by problem_num );`, function(err,result2){
-      var html = '';
-      leng1 = 0;
-      leng2 = 0;  
-      if(result1 != undefined){
-        leng1 = result1.length;
+    db.query(`SELECT * FROM essay_p where problem_num in (select problem_num from essay_a where adviser_id = ${req.session.user_id} group by problem_num );`, function(err,result2){ 
+      if(result1 === undefined){
+        result1 = [];
       } 
-      if(result2 != undefined){
-        leng2 = result2.length;
+      if(result2 === undefined){
+        result2 = [];
       }
-      var html = template.My_page(leng1, leng2, result1, result2);
-      res.send(html);
+      leng1 = result1.length;
+      leng2 = result2.length;
+      var html = template.My_page(leng1, leng2, result1, result2, username);
+      res.send(html); 
     });
   });
 
-  
+
 });
 
 router.post('/My_page2',function(req,res){
   index = Object.keys(req.body)[0]-1;
+  console.log(req.body);
+  console.log(index);
   if(index < leng1){
     db.query(`select * from essay_s inner join members on essay_s.user_id = members.user_id where problem_num = (SELECT problem_num FROM essay_p where school = '${req.body.school[index]}' and year = ${req.body.year[index]}) and essay_s.user_id = ${req.session.user_id};`,function(err,result1){
       db.query(`select * from essay_a inner join members on adviser_id = user_id where problem_num = ${result1[0].problem_num} and author_id = ${req.session.user_id};`,function(err,result2){
         temp_num = result1[0].problem_num;
-        var html = template.My_page2(result1.length, result2.length, result1, result2)
+        var html = template.My_page2(result1.length, result2.length, result1, result2, username)
         res.send(html);
       });
     });
@@ -177,7 +178,7 @@ router.post('/My_page2',function(req,res){
       db.query(`select * from essay_s inner join members on essay_s.user_id = members.user_id where problem_num = ${result1[0].problem_num} and essay_s.user_id = ${result1[0].author_id};`,function(err,result2){
         db.query(`select * from essay_a inner join members on adviser_id = user_id where problem_num = ${result2[0].problem_num} and author_id = ${result2[0].user_id};`,function(err,result3){
           temp_num = result1[0].problem_num; 
-          var html = template.My_page2(result2.length, result3.length, result2, result3)
+          var html = template.My_page2(result2.length, result3.length, result2, result3, username)
           res.send(html);
         }); 
       });
